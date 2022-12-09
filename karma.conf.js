e951a5c786e8df4d5e415737ec9f64cf84e74e03
Jessica Chen <calmdowngirl.au@gmail.com>
2022-12-09 15:01:52 +0930

    init

---
+++ karma.conf.js
@@ -0,0 +1,11 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ["jasmine"],
+    files: ["src/**/*.js", "test/**/*_spec.js"],
+    preprocessors: {
+      "test/**/*.js": ["jshint"],
+      "src/**/*.js": ["jshint"],
+    },
+    browsers: ["PhamtomJS"],
+  });
+};
